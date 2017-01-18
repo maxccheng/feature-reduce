6df84a3a8df5216db59a983446a8a156a38c8201
popo <maxccheng@gmail.com>
2017-01-18 17:03:54 +0800

    Add repeats and avg scores to hold out validation

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 """Execute wrapper method feature selection on 12 UCI datasets with DecisionTreeClassifier"""
 print(__doc__)
 
+# suppress warnings e.g. divide by 0 in precision_recall_fscore_support()
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,6 +14,7 @@ from matplotlib.colors import ListedColormap
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import precision_recall_fscore_support
 
 def feature_select(feature, decision):
     return feature
@@ -25,19 +32,30 @@ for i, f in enumerate(os.listdir(dsets_path)):
         # do feature selection
         X = feature_select(X, y)   
 
-        # split into training and test sets randomly with defined size
-        X_train, X_test, y_train, y_test = \
-            train_test_split(X, y, test_size=0.20, random_state=20) 
+        metrics = []
+        rep = 10
+        for j in range(rep):
+            # hold out split with ratio 80 training : 20 test, repeated randomly for 10 times
+            X_train, X_test, y_train, y_test = \
+                train_test_split(X, y, test_size=0.20) 
 
-        # train classifier
-        clf = DecisionTreeClassifier()
-        clf.fit(X_train, y_train) 
-                
-        # verify with test sets 
-        score = clf.score(X_test, y_test)        
+            # train classifier
+            clf = DecisionTreeClassifier()
+            clf.fit(X_train, y_train) 
 
-        # calculate classification score
-        print "%02d %s train_set_size=%s/%s score=%.2f" % (i, f, X_train.shape[0], X.shape[0], score)
+            # predict with test sets and save scores
+            y_true = y_test
+            y_predict = clf.predict(X_test)
+            score = precision_recall_fscore_support(y_true, y_predict, average='macro')
+            score = score[:-1]
+            metrics = np.append(metrics, score) 
+
+        # average 10 repeat scores
+        metrics = metrics.reshape(rep, 3)
+        metrics_avg = np.mean(metrics, axis=0) 
+
+        # print average scores
+        print "%02d %-15s dim_selected=%02d train_instances=%5s/%5s precision=%.2f recall=%.2f fscore=%.2f" % (i, f, X.shape[1], X_train.shape[0], X.shape[0], metrics_avg[0], metrics_avg[1], metrics_avg[2])
 
 #h = .02  # step size in the mesh
 #
