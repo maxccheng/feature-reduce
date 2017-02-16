cc0127a8638b9b0c0ab4c1f9b7658ea88ddf6598
popo <maxccheng@gmail.com>
2017-02-16 17:15:40 +0800

    Change each algorithm to module form

--- beta_hill_climb.py
+++ beta_hill_climb.py
@@ -1,9 +1,7 @@
-"""Feature selection on 11 UCI datasets to minimize feature count and maximize scores"""
 """Beta hill climbing and forward search"""
 """Termination criteria: degree of dependency == 1.0"""
 """Mutation chance: 5%"""
 """Mutation bit length: randomize 1 bit/feature"""
-print(__doc__)
 
 # suppress warnings e.g. divide by 0 in precision_recall_fscore_support()
 def warn(*args, **kwargs):
@@ -80,44 +78,4 @@ def beta_hill_climb(feature, decision, search_type = "forward_search", mutate_ch
                 fitn_best = degree_dependency(feature[:,solution], decision) 
 
     return feature[:,solution]
-
-dsets_path = "./datasets/"
-dset_ext = ".dat"
-for i, f in enumerate(os.listdir(dsets_path)):
-    if f.endswith(dset_ext): 
-        ds = np.loadtxt(dsets_path + f)
-        ds = ds.astype(int)
-        # split into feature and decision with rows as instances
-        X = ds[:, :-1] 
-        y = ds[:, -1].reshape(-1, 1)
-
-        # do feature selection
-        X_all_count = X.shape[1]
-        X = beta_hill_climb(X, y)   
-        #np.set_printoptions(threshold=100000)
-
-        metrics = []
-        rep = 10
-        for j in xrange(rep):
-            # hold out split with ratio 80 training : 20 test, repeated randomly for 10 times
-            X_train, X_test, y_train, y_test = \
-                train_test_split(X, y, test_size=0.20) 
-
-            # train classifier
-            clf = DecisionTreeClassifier()
-            clf.fit(X_train, y_train) 
-
-            # predict with test sets and save scores
-            y_true = y_test
-            y_predict = clf.predict(X_test)
-            score = precision_recall_fscore_support(y_true, y_predict, average='macro')
-            score = score[:-1]
-            metrics = np.append(metrics, score) 
-
-        # average 10 repeat scores
-        metrics = metrics.reshape(rep, 3)
-        metrics_avg = np.mean(metrics, axis=0) 
-
-        # print average scores
-        print "%02d %-15s dim_selected=%02d/%02d train_instances=%5s/%5s precision=%.2f recall=%.2f fscore=%.2f" % (i, f, X.shape[1], X_all_count, X_train.shape[0], X.shape[0], metrics_avg[0], metrics_avg[1], metrics_avg[2])
 
--- greedy_hill_climb.py
+++ greedy_hill_climb.py
@@ -1,7 +1,5 @@
-"""Feature selection on 11 UCI datasets to minimize feature count and maximize scores"""
 """Greedy hill climbing, forward search"""
 """Termination criteria: degree of dependency == 1.0"""
-print(__doc__)
 
 # suppress warnings e.g. divide by 0 in precision_recall_fscore_support()
 def warn(*args, **kwargs):
@@ -72,44 +70,4 @@ def greedy_hill_climb(feature, decision, search_type = "forward_search"):
             solution[feat_best] = True  # add the fittest feature
 
     return feature[:,solution]
-
-dsets_path = "./datasets/"
-dset_ext = ".dat"
-for i, f in enumerate(os.listdir(dsets_path)):
-    if f.endswith(dset_ext): 
-        ds = np.loadtxt(dsets_path + f)
-        ds = ds.astype(int)
-        # split into feature and decision with rows as instances
-        X = ds[:, :-1] 
-        y = ds[:, -1].reshape(-1, 1)
-
-        # do feature selection
-        X_all_count = X.shape[1]
-        X = greedy_hill_climb(X, y)   
-        #np.set_printoptions(threshold=100000)
-
-        metrics = []
-        rep = 10
-        for j in xrange(rep):
-            # hold out split with ratio 80 training : 20 test, repeated randomly for 10 times
-            X_train, X_test, y_train, y_test = \
-                train_test_split(X, y, test_size=0.20) 
-
-            # train classifier
-            clf = DecisionTreeClassifier()
-            clf.fit(X_train, y_train) 
-
-            # predict with test sets and save scores
-            y_true = y_test
-            y_predict = clf.predict(X_test)
-            score = precision_recall_fscore_support(y_true, y_predict, average='macro')
-            score = score[:-1]
-            metrics = np.append(metrics, score) 
-
-        # average 10 repeat scores
-        metrics = metrics.reshape(rep, 3)
-        metrics_avg = np.mean(metrics, axis=0) 
-
-        # print average scores
-        print "%02d %-15s dim_selected=%02d/%02d train_instances=%5s/%5s precision=%.2f recall=%.2f fscore=%.2f" % (i, f, X.shape[1], X_all_count, X_train.shape[0], X.shape[0], metrics_avg[0], metrics_avg[1], metrics_avg[2])
 
